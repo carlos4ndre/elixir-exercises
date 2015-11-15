c6bb0797be633d45a407ba2d2d9f141cb0e182d3
carlos4ndre <carlos.ulrich@gmail.com>
2015-11-15 23:34:30 +0000

    Add solution for the exercise 1 in Chapter 14

---
+++ chapter-14/working-with-multiple-processes-1/chain.exs
@@ -0,0 +1,29 @@
+defmodule Chain do
+  def counter(next_pid) do
+    receive do
+      n ->
+        send next_pid, n + 1
+    end
+  end
+
+  def create_processes(n) do
+    last = Enum.reduce 1..n, self,
+            fn (_, send_to) ->
+              spawn(Chain, :counter, [send_to])
+            end
+
+    # start the count by sending
+    send last, 0
+
+    # and wait for the result to come back to us
+    receive do
+      final_answer when is_integer(final_answer) ->
+        "Result is #{inspect(final_answer)}"
+    end
+  end
+
+  def run(n) do
+    IO.puts inspect :timer.tc(Chain, :create_processes, [n])
+  end
+end
+
