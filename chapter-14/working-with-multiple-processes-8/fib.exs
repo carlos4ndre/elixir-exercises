7d621edddfe7b37d1409830c0cbddddd119b4af8
carlos4ndre <carlos.ulrich@gmail.com>
2015-12-05 16:25:27 +0000

    Add solution for the exercise 8 in Chapter 14

---
+++ chapter-14/working-with-multiple-processes-8/fib.exs
@@ -0,0 +1,77 @@
+defmodule FibSolver do
+
+  def fib(scheduler) do
+    send scheduler, { :ready, self }
+    receive do
+      { :fib, n, client } ->
+        send client, { :answer, n, fib_calc(n), self }
+        fib(scheduler)
+      { :shutdown } ->
+        exit(:normal)
+    end
+  end
+
+  defp fib_calc(0), do: 0
+  defp fib_calc(1), do: 1
+  defp fib_calc(n), do: fib_calc(n-1) + fib_calc(n-2)
+end
+
+defmodule Scheduler do
+
+  def run(num_processes, module, func, to_calculate) do
+    (1..num_processes)
+    |> Enum.map(fn(_) -> spawn(module, func, [self]) end)
+    |> schedule_processes(to_calculate, [])
+  end
+
+  defp schedule_processes(processes, queue, results) do
+    receive do
+      {:ready, pid} when length(queue) > 0 ->
+        [ next | tail ] = queue
+        send pid, {:fib, next, self}
+        schedule_processes(processes, tail, results)
+
+      {:ready, pid} ->
+        send pid, {:shutdown}
+        if length(processes) > 1 do
+          schedule_processes(List.delete(processes, pid), queue, results)
+        else
+          Enum.sort(results, fn {n1,_}, {n2,_} -> n1 <= n2 end)
+        end
+
+      {:answer, number, result, _pid} ->
+        schedule_processes(processes, queue, [{number, result} | results])
+    end
+  end
+end
+
+to_process = [ 37, 37, 37, 37, 37, 37 ]
+
+Enum.each 1..10, fn num_processes ->
+  {time, result} = :timer.tc(Scheduler, :run,
+                              [num_processes, FibSolver, :fib, to_process])
+  if num_processes == 1 do
+    IO.puts inspect result
+    IO.puts "\n #   time (s)"
+  end
+  :io.format "~2B    ~.2f~n", [num_processes, time/1000000.0]
+end
+
+"""
+$ sysctl hw.ncpu
+hw.ncpu: 4
+$ elixir fib.exs
+[{37, 24157817}, {37, 24157817}, {37, 24157817}, {37, 24157817}, {37, 24157817}, {37, 24157817}]
+
+ #   time (s)
+ 1    12.24
+ 2    6.41
+ 3    5.54
+ 4    7.22
+ 5    5.40
+ 6    4.64
+ 7    4.65
+ 8    4.64
+ 9    4.64
+10    4.61
+"""
