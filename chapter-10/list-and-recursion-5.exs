deaef8d1517d165567d6fad0b9da038ac559e881
Carlos André <carlos.ulrich@gmail.com>
2017-08-21 06:09:59 +0100

    Fix take method with negative indexes

--- chapter-10/list-and-recursion-5.exs
+++ chapter-10/list-and-recursion-5.exs
@@ -24,14 +24,24 @@ defmodule MyEnum do
   defp _split([head | tail], n, result) when length(result) < n do
     _split(tail, n, result ++ [head])
   end
-  defp _split(list, n, result), do: { result, list }
+  defp _split(list, _n, result), do: { result, list }
+
+  def reverse([]), do: []
+  def reverse([head | tail]), do: reverse(tail) ++ [head]
 
   def take(list, n) when n >= 0, do: _take(list, n, [])
-  def take(list, n) when n < 0, do: _take(list, n + length(list), [])
+  def take(list, n) when n < 0 do
+    positive_index = abs(n)
+
+    list
+    |> reverse()
+    |> _take(positive_index, [])
+    |> reverse()
+  end
   defp _take([head | tail], n, result) when length(result) < n do
     _take(tail, n, result ++ [head])
   end
-  defp _take(list, n, result), do: result
+  defp _take(_list, _n, result), do: result
 end
 
 IO.inspect MyEnum.all?([1,2,3,4], &(&1 > 0)) # true
@@ -50,11 +60,17 @@ IO.inspect MyEnum.split([1,2,3,4], 0)  # {[], [1,2,3,4]}
 IO.inspect MyEnum.split([1,2,3,4], -1) # {[1,2,3], [4]}
 IO.inspect MyEnum.split([1,2,3,4], -5) # {[], [1,2,3,4]}
 
+IO.inspect MyEnum.reverse([])                       # []
+IO.inspect MyEnum.reverse([1])                      # [1]
+IO.inspect MyEnum.reverse([1, "two"])               # ["two",1]
+IO.inspect MyEnum.reverse([1, "two", :three])       # [:three,"two,1]
+IO.inspect MyEnum.reverse([1, "two", :three, 4.0])  # [4.0,:three,"two",1]
+
 IO.inspect MyEnum.take([1,2,3,4], 2)  # [1,2]
 IO.inspect MyEnum.take([1,2,3,4], 10) # [1,2,3,4]
 IO.inspect MyEnum.take([1,2,3,4], 0)  # []
-IO.inspect MyEnum.take([1,2,3,4], -1) # [1,2,3]
-IO.inspect MyEnum.take([1,2,3,4], -5) # []
+IO.inspect MyEnum.take([1,2,3,4], -1) # [4]
+IO.inspect MyEnum.take([1,2,3,4], -5) # [1,2,3,4]
 
 IO.inspect MyEnum.each(["john", "doe"], &(IO.puts &1))
 # john
