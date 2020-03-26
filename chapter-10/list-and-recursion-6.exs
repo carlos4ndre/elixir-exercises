3673cf69d08a6c7de53224f8956f0db21e7c5dad
Carlos André <carlos.andre@ubisoft.com>
2020-03-25 20:05:39 -0400

    Improve flatten function using is_list guard clause

--- chapter-10/list-and-recursion-6.exs
+++ chapter-10/list-and-recursion-6.exs
@@ -1,12 +1,9 @@
 defmodule MyEnum do
   def flatten([]), do: []
-  def flatten([head | last]) do
-    if is_list(head) do
-      flatten(head) ++ flatten(last)
-    else
-      [head] ++ flatten(last)
-    end
+  def flatten([head | tail]) when is_list(head) do
+    flatten(head) ++ flatten(tail)
   end
+  def flatten([head | tail]), do: [head] ++ flatten(tail)
 end
 
 IO.inspect MyEnum.flatten([1,[2,3,[4]],5,[[[6]]]]) # [1,2,3,4,5,6]
